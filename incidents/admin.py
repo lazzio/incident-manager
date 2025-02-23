4737bcb59b8ec0a39ffa9964c4c2182525b80000
lazzio <mat@myframboise.com>
2025-02-23 18:14:57 +0100

    fix: display fields values (#9)

--- incidents/admin.py
+++ incidents/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Incident, IncidentAttachment, IncidentLink, IncidentUpdate
 
-# Register your models here.
+
+@admin.register(Incident, IncidentAttachment, IncidentLink, IncidentUpdate)
+class IncidentAdmin(admin.ModelAdmin):
+    pass
+
+class IncidentAttachmentAdmin(admin.ModelAdmin):
+    pass
+
+class IncidentLinkAdmin(admin.ModelAdmin):
+    pass
+
+class IncidentUpdateAdmin(admin.ModelAdmin):
+    pass
